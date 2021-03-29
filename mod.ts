c63a811b0073e9e0e8b7f025fe8be7eda6e68b5b
koyasaeki <koyasaeki@gmail.com>
2021-03-29 22:40:14 +0900

    :sparkles: 表示メッセージの変更

--- mod.ts
+++ mod.ts
@@ -1,44 +1,11 @@
-function handleRequest(request) {
-  const { pathname } = new URL(request.url);
-
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
-
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-
+function handleRequest(request: any) {
   return new Response(
     `<body
       align="center"
       style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
     >
       <p>
-        Visit <a href="/json">/json</a> for JSON response and
-        <a href="/html">/html</a> for HTML response
+        Hello Deno Deploy!
       </p>
     </body>`,
     {
@@ -49,6 +16,6 @@ function handleRequest(request) {
   );
 }
 
-addEventListener("fetch", (event) => {
+addEventListener("fetch", (event: any) => {
   event.respondWith(handleRequest(event.request));
 });
